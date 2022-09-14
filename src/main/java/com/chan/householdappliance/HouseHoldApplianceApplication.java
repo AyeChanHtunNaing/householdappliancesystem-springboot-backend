ae46226fec12a9fe9a208b13eb154306cf0124c4
AyeChanHtunNaing <ayechanhtunnaing@icloud.com>
2022-09-14 18:46:06 +0630

    <AC> added model and database layer

--- src/main/java/com/chan/householdappliance/HouseHoldApplianceApplication.java
+++ src/main/java/com/chan/householdappliance/HouseHoldApplianceApplication.java
@@ -2,8 +2,10 @@ package com.chan.householdappliance;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class HouseHoldApplianceApplication {
 
 	public static void main(String[] args) {
