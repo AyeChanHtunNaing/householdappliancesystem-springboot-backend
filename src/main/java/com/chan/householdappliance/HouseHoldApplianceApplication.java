0aa4f6c6752e52e3d53c78a5cef75bc9d0e78cdd
AyeChanHtunNaing <ayechanhtunnaing@icloud.com>
2022-09-14 13:47:57 +0630

    <AC> started the project

---
+++ src/main/java/com/chan/householdappliance/HouseHoldApplianceApplication.java
@@ -0,0 +1,13 @@
+package com.chan.householdappliance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseHoldApplianceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouseHoldApplianceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chan/householdappliance/HouseHoldApplianceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chan.householdappliance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseHoldApplianceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
